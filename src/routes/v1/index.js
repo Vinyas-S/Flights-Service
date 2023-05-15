1e041c0ee7f5e6c45e1071d6176a7e9f39077954
Vinyas S <svinyas2000@gmail.com>
2023-05-15 19:59:34 +0530

    added routes contollers services repos

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,7 +2,11 @@ const express = require('express');
 
 const router =express.Router();
 
-const {InfoController} = require('../../controllers/index')
-router.get('/info', InfoController.info)
+const {InfoController} = require('../../controllers/')
 
+const airplaneRoutes = require('./airplane-routes')
+
+router.use('/airplanes',airplaneRoutes)
+
+router.get('/info', InfoController.info)
 module.exports = router
