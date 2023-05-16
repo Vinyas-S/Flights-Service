e837dc96cbdaf4f685ce732ca04403ff31292219
Vinyas S <svinyas2000@gmail.com>
2023-05-16 18:38:28 +0530

    added city crud apis

--- src/controllers/city-controller.js
+++ src/controllers/city-controller.js
@@ -1,14 +1,13 @@
-const {AirplaneService} = require('../services');
-const {StatusCodes}= require("http-status-codes")
+const {CityService} = require('../services');
+const {StatusCodes}= require('http-status-codes')
 const {SuccessResponse , ErrorResponse} = require('../utils/common')
 
-async function createAirplane(req,res){
+async function createCity(req,res){
     try {
-        const airplane =await AirplaneService.createAirplane({
-            modelNumber : req.body.modelNumber,
-            capacity : req.body.capacity
+        const city =await CityService.createCity({
+            name: req.body.name
         })
-        SuccessResponse.data = airplane
+        SuccessResponse.data = city
         return res
                 .status(StatusCodes.CREATED)
                 .json(SuccessResponse)
@@ -20,10 +19,10 @@ async function createAirplane(req,res){
     }
 }
 
-async function getAirplanes(req,res){
+async function getCities(req,res){
     try {
-        const airplanes = await AirplaneService.getAirplanes();
-        SuccessResponse.data = airplanes;
+        const cities = await CityService.getCities();
+        SuccessResponse.data = cities;
         return res
                 .status(StatusCodes.OK)
                 .json(SuccessResponse)
@@ -35,16 +34,12 @@ async function getAirplanes(req,res){
     }
 }
 
-/**
- * 
- * POST : /airplanes/:id
- * req.body : {}
- */
 
-async function getAirplane(req,res){
+
+async function getCity(req,res){
     try {
-        const airplanes = await AirplaneService.getAirplane(req.params.id);
-        SuccessResponse.data = airplanes;
+        const city = await CityService.getCity(req.params.id);
+        SuccessResponse.data = city;
         return res
                 .status(StatusCodes.OK)
                 .json(SuccessResponse)
@@ -62,10 +57,10 @@ async function getAirplane(req,res){
  * req.body : {}
  */
 
-async function destroyAirplane(req,res){
+async function destroyCity(req,res){
     try {
-        const airplanes = await AirplaneService.destroyAirplane(req.params.id);
-        SuccessResponse.data = airplanes;
+        const city = await CityService.destroyCity(req.params.id);
+        SuccessResponse.data = city;
         return res
                 .status(StatusCodes.OK)
                 .json(SuccessResponse)
@@ -78,10 +73,10 @@ async function destroyAirplane(req,res){
 }
 
 
-async function updateAirplane(req,res){
+async function updateCity(req,res){
     try {
-        const airplanes = await AirplaneService.updateAirplane(req.params.id,req.body);
-        SuccessResponse.data = airplanes;
+        const city = await CityService.updateCity(req.params.id,req.body);
+        SuccessResponse.data = city;
         return res
                 .status(StatusCodes.OK)
                 .json(SuccessResponse)
@@ -96,9 +91,11 @@ async function updateAirplane(req,res){
 
 
 module.exports = {
-    createAirplane,
-    getAirplanes,
-    getAirplane,
-    destroyAirplane,
-    updateAirplane
+    createCity,
+    getCities,
+    getCity,
+    destroyCity,
+    updateCity
+
 }
+
---
+++ src/routes/v1/city-routes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const {CityController} = require('../../controllers')
+const {CityMiddlewares} = require('../../middlewares/')
+router
+    .post('/',
+    CityMiddlewares.validateCreateRequest,
+    CityController.createCity);
+
+router.get('/', CityController.getCities);
+
+router.get('/:id',CityController.getCity);
+
+router.delete('/:id',CityController.destroyCity);
+
+router.patch('/:id', CityController.updateCity);
+
+module.exports = router;
