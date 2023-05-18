d7c35734ea6e8aff96844028bb3820a7de4aea9f
Vinyas S <svinyas2000@gmail.com>
2023-05-18 12:10:15 +0530

    added flights model with foreign key constraint defined during model definition

--- src/index.js
+++ src/index.js
@@ -1,36 +1,15 @@
-const express = require('express')
-const {ServerConfig, Logger} =require('./config')
-const apiRoutes = require('./routes')
+const express = require("express");
+const { ServerConfig, Logger } = require("./config");
+const apiRoutes = require("./routes");
 
-const app =express();
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/api',apiRoutes);
+app.use("/api", apiRoutes);
 
-app.listen(ServerConfig.PORT,async()=>{
-    console.log(`server started on ${ServerConfig.PORT}`);
-    Logger.info("Sucessfully started server ",{})
-    
-    const {Airport, City}  = require('./models');
-    // const cities = await City.findByPk(1);
-    // console.log(cities);
-    // const airport  = await Airport.create({name:'Kempegowda airport', code:'BLR',})
-    // console.log(airport);
-    // const response = await cities.createAirport({name:'Hubbali airport', code:'HBL',})
-    // console.log(response)
-    // const airports = await cities.getAirports();
-    // console.log(airports);
-    // const response = await Airport.findByPk(6);
-    // console.log(response)
-    // await cities.removeAirport(response);
-    await City.destroy({
-        where:{
-            id:1
-        }
-    })
+app.listen(ServerConfig.PORT, () => {
+  console.log(`server started on ${ServerConfig.PORT}`);
 });
-
-//bad code alert
 
--- src/models/airport.js
+++ src/models/airport.js
@@ -16,6 +16,16 @@ module.exports = (sequelize, DataTypes) => {
         onDelete: 'CASCADE',
        
        });
+
+       this.hasMany(models.Flights,{
+        foreignKey:'departureAirportId',
+        onDelete: 'CASCADE',
+       })
+
+       this.hasMany(models.Flights,{
+        foreignKey:'arrivalAirportId',
+        onDelete: 'CASCADE',
+       })
     }
   }
   Airport.init({
